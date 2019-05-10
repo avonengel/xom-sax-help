bc1aef6c69cebc96c8f084c89f00d87d5bcfcaaa
Axel von Engel <a.vonengel@gmail.com>
2019-05-10 18:42:56 +0200

    Add test that checks for Image not found message

--- src/main/java/com/github/avonengel/xomsaxhelp/XslFo.java
+++ src/main/java/com/github/avonengel/xomsaxhelp/XslFo.java
@@ -2,16 +2,20 @@ package com.github.avonengel.xomsaxhelp;
 
 import nu.xom.Builder;
 import nu.xom.Document;
+import nu.xom.Nodes;
 import nu.xom.converters.SAXConverter;
 import nu.xom.xinclude.XIncluder;
-import org.apache.avalon.framework.configuration.DefaultConfigurationBuilder;
+import nu.xom.xslt.XSLTransform;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
 import org.apache.fop.apps.FopFactoryBuilder;
 import org.apache.fop.apps.MimeConstants;
+import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParserFactory;
 import javax.xml.transform.Result;
 import javax.xml.transform.sax.SAXResult;
@@ -22,7 +26,9 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.OutputStream;
+import java.io.Writer;
 import java.lang.reflect.Field;
 import java.util.LinkedList;
 import java.util.List;
@@ -42,33 +48,52 @@ public class XslFo
         this.outputFile = outputFile;
     }
 
-    void convertPdf() throws Exception
+    void convertPdfWithXom() throws Exception
     {
-        System.setProperty("javax.xml.parsers.SAXParserFactory", "org.apache.xerces.jaxp.SAXParserFactoryImpl");
-        final SAXParserFactory parserFactory = SAXParserFactory.newInstance("org.apache.xerces.jaxp.SAXParserFactoryImpl", XslFo.class.getClassLoader());
-
-        parserFactory.setNamespaceAware(true);
-        parserFactory.setXIncludeAware(false);
-        parserFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-        parserFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
-        //            parserFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
-
-        XMLReader xmlReader = parserFactory.newSAXParser().getXMLReader();
+        XMLReader xmlReader = getXmlReader(false);
 
         // Use XOM to resolve xi:include: Xerces does not handle nested xincludes correctly
         // found this: https://sourceforge.net/p/docbook/bugs/1327/
         // which points to this: https://issues.apache.org/jira/browse/XERCESJ-1102
         Builder parser = new Builder(xmlReader);
         Document input = parser.build(rootDocbookFile);
+        XIncluder.resolveInPlace(input, parser);
 
-        // XOM's SAXConverter does not produce xml:base, so we have to write to a temporary file
-        input = XIncluder.resolve(input, parser);
-        //        File xincluded = outputFile.get().getAsFile().toPath().resolveSibling("xincluded.xml").toFile();
-        //        try (FileWriter writer = new FileWriter(xincluded))
-        //        {
-        //            writer.write(input.toXML());
-        //        }
-        //        Source source = new SAXSource(xmlReader, new InputSource(xincluded.toURI().toASCIIString()));
+        LinkedList<TransformerHandler> transformerHandlers = new LinkedList<>();
+        for (int i = 0; i < xslFiles.size(); i++)
+        {
+            File xslFile = xslFiles.get(i);
+            final Document transformationDocument = parser.build(xslFile);
+            final Nodes nodes = new XSLTransform(transformationDocument).transform(input);
+            input = XSLTransform.toDocument(nodes);
+            if (outputIntermediateResults)
+            {
+                File intermediate = outputFile.toPath().resolveSibling(i + xslFile.getName() + ".xml").toFile();
+                try (Writer writer = new FileWriter(intermediate))
+                {
+                    writer.write(input.toXML());
+                }
+            }
+        }
+
+        FopFactory fopFactory = new FopFactoryBuilder(rootDocbookFile.getParentFile().toURI()).build();
+        try (OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFile)))
+        {
+            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);
+            buildSaxConverter(false, fop.getDefaultHandler()).convert(input);
+        }
+    }
+
+    void convertPdfWithXomAndSaxon(boolean applyXmlBaseWorkaround) throws Exception
+    {
+        XMLReader xmlReader = getXmlReader(false);
+
+        // Use XOM to resolve xi:include: Xerces does not handle nested xincludes correctly
+        // found this: https://sourceforge.net/p/docbook/bugs/1327/
+        // which points to this: https://issues.apache.org/jira/browse/XERCESJ-1102
+        Builder parser = new Builder(xmlReader);
+        Document input = parser.build(rootDocbookFile);
+        XIncluder.resolveInPlace(input, parser);
 
         SAXTransformerFactory transformerFactory = (SAXTransformerFactory) SAXTransformerFactory.newInstance("net.sf.saxon.jaxp.SaxonTransformerFactory", XslFo.class.getClassLoader());
 
@@ -82,18 +107,11 @@ public class XslFo
             if (outputIntermediateResults)
             {
                 File intermediate = outputFile.toPath().resolveSibling(i + xslFile.getName() + ".xml").toFile();
-                transformerHandler.setResult(new StreamResult(intermediate));
 
                 if (i == 0)
                 {
-                    TransformerHandler handler = transformerFactory.newTransformerHandler(transformationSource);
-                    handler.setResult(new StreamResult(intermediate));
-                    //                    transformerFactory.newTransformer().transform(source, new StreamResult(intermediate));
-                    SAXConverter saxConverter = new SAXConverter(handler);
-                    Field stripBaseAttributes = SAXConverter.class.getDeclaredField("stripBaseAttributes");
-                    stripBaseAttributes.setAccessible(true);
-                    stripBaseAttributes.setBoolean(saxConverter, false);
-                    saxConverter.convert(input);
+                    transformerHandler.setResult(new StreamResult(intermediate));
+                    buildSaxConverter(applyXmlBaseWorkaround, transformerHandler).convert(input);
                 }
                 else
                 {
@@ -108,7 +126,6 @@ public class XslFo
             transformerHandlers.add(transformerHandler);
         }
 
-        DefaultConfigurationBuilder cfgBuilder = new DefaultConfigurationBuilder();
         FopFactory fopFactory = new FopFactoryBuilder(rootDocbookFile.getParentFile().toURI()).build();
         try (OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFile)))
         {
@@ -125,8 +142,87 @@ public class XslFo
             else
             {
                 lastTransformerHandler.setResult(foResult);
-                new SAXConverter(firstTransformerHandler).convert(input);
+                buildSaxConverter(applyXmlBaseWorkaround, firstTransformerHandler).convert(input);
             }
         }
+    }
+
+    void convertPdfWithXercesInclude() throws Exception
+    {
+        // set xIncludeAware on the Xerces parser to true
+        // does not resolve nested xIncludes correctly: https://issues.apache.org/jira/browse/XERCESJ-1102
+        XMLReader xmlReader = getXmlReader(true);
+
+        SAXTransformerFactory transformerFactory = (SAXTransformerFactory) SAXTransformerFactory.newInstance("net.sf.saxon.jaxp.SaxonTransformerFactory", XslFo.class.getClassLoader());
+
+        // TODO: refactor the ugly intermediate output logic
+        LinkedList<TransformerHandler> transformerHandlers = new LinkedList<>();
+        for (int i = 0; i < xslFiles.size(); i++)
+        {
+            File xslFile = xslFiles.get(i);
+            SAXSource transformationSource = new SAXSource(xmlReader, new InputSource(xslFile.toURI().toASCIIString()));
+            if (outputIntermediateResults)
+            {
+                final StreamResult intermediateResult = new StreamResult(outputFile.toPath().resolveSibling(i + xslFile.getName() + ".xml").toFile());
+
+                File intermediateInputFile = rootDocbookFile;
+                if (i > 0)
+                {
+                    intermediateInputFile = outputFile.toPath().resolveSibling((i - 1) + xslFiles.get(i - 1).getName() + ".xml").toFile();
+                }
+                transformerFactory.newTransformer(transformationSource).transform(new SAXSource(xmlReader, new InputSource(intermediateInputFile.toURI().toASCIIString())), intermediateResult);
+            }
+            else if (i > 0)
+            {
+                TransformerHandler transformerHandler = transformerFactory.newTransformerHandler(transformationSource);
+                transformerHandlers.getLast().setResult(new SAXResult(transformerHandler));
+                transformerHandlers.add(transformerHandler);
+            }
+        }
+
+        FopFactory fopFactory = new FopFactoryBuilder(rootDocbookFile.getParentFile().toURI()).build();
+        try (OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFile)))
+        {
+            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);
+            Result foResult = new SAXResult(fop.getDefaultHandler());
+            if (outputIntermediateResults)
+            {
+                int index = xslFiles.size() - 1;
+                File lastIntermediate = outputFile.toPath().resolveSibling(index + xslFiles.get(index).getName() + ".xml").toFile();
+                transformerFactory.newTransformer().transform(new SAXSource(xmlReader, new InputSource(lastIntermediate.toURI().toASCIIString())), foResult);
+            }
+            else
+            {
+                transformerHandlers.getLast().setResult(foResult);
+                xmlReader.setContentHandler(transformerHandlers.getFirst());
+                xmlReader.parse(rootDocbookFile.toURI().toASCIIString());
+            }
+        }
+    }
+
+    private SAXConverter buildSaxConverter(boolean applyXmlBaseWorkaround, ContentHandler handler) throws NoSuchFieldException, IllegalAccessException
+    {
+        SAXConverter saxConverter = new SAXConverter(handler);
+        if (applyXmlBaseWorkaround)
+        {
+            Field stripBaseAttributes = SAXConverter.class.getDeclaredField("stripBaseAttributes");
+            stripBaseAttributes.setAccessible(true);
+            stripBaseAttributes.setBoolean(saxConverter, false);
+        }
+        return saxConverter;
+    }
+
+    private XMLReader getXmlReader(boolean includeAware) throws ParserConfigurationException, SAXException
+    {
+        System.setProperty("javax.xml.parsers.SAXParserFactory", "org.apache.xerces.jaxp.SAXParserFactoryImpl");
+        final SAXParserFactory parserFactory = SAXParserFactory.newInstance("org.apache.xerces.jaxp.SAXParserFactoryImpl", XslFo.class.getClassLoader());
+
+        parserFactory.setNamespaceAware(true);
+        parserFactory.setXIncludeAware(includeAware);
+        parserFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        parserFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+        //            parserFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+
+        return parserFactory.newSAXParser().getXMLReader();
     }
 }
--- src/test/java/com/github/avonengel/xomsaxhelp/XslFoTest.java
+++ src/test/java/com/github/avonengel/xomsaxhelp/XslFoTest.java
@@ -1,21 +1,30 @@
 package com.github.avonengel.xomsaxhelp;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.io.FileMatchers.anExistingFile;
 
 class XslFoTest
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(XslFoTest.class);
     private File foXslFile;
     private File docbookFile;
     @TempDir
@@ -37,17 +46,82 @@ class XslFoTest
         foXslFile = new File(foXslFileUrl.toURI());
     }
 
+    @BeforeEach
+    void clearStaticAppender()
+    {
+        StaticAppender.clearEvents();
+    }
+
+    Path getTempDir(String subDirectory) {
+        final String testTempDir = System.getenv("TEST_TEMP_DIR");
+        if (testTempDir != null)
+        {
+            final Path dir = Paths.get(testTempDir).resolve(subDirectory);
+            LOGGER.info("Test temp directory: {}", dir);
+            dir.toFile().mkdirs();
+            return dir;
+        } else
+        {
+            return tempDir;
+        }
+    }
+
     @Test
-    void convertPdf() throws Exception
+    void convertPdfWithXomSaxonAndWorkaround() throws Exception
     {
         // Arrange
-        final File outputFile = tempDir.resolve(docbookFile.getName() + ".pdf").toFile();
+        final File outputFile = getTempDir("xomSaxonWithWorkaround").resolve(docbookFile.getName() + ".pdf").toFile();
         final XslFo xslFo = new XslFo(Collections.singletonList(foXslFile), docbookFile, false, outputFile);
 
         // Act
-        xslFo.convertPdf();
+        xslFo.convertPdfWithXomAndSaxon(true);
 
         // Assert
-        assertThat(outputFile, anExistingFile());
+        final List<ILoggingEvent> events = StaticAppender.getEvents();
+        assertThat(events, not(hasItem(hasProperty("message", containsString("Image not found")))));
+    }
+    @Test
+    void convertPdfWithXom() throws Exception
+    {
+        // Arrange
+        final File outputFile = getTempDir("xom").resolve(docbookFile.getName() + ".pdf").toFile();
+        final XslFo xslFo = new XslFo(Collections.singletonList(foXslFile), docbookFile, false, outputFile);
+
+        // Act
+        xslFo.convertPdfWithXom();
+
+        // Assert
+        final List<ILoggingEvent> events = StaticAppender.getEvents();
+        assertThat(events, not(hasItem(hasProperty("message", containsString("Image not found")))));
+    }
+
+    @Test
+    void convertPdfWithXomSaxonWithoutWorkaround() throws Exception
+    {
+        // Arrange
+        final File outputFile = getTempDir("xomSaxonWithoutWorkaround").resolve(docbookFile.getName() + ".pdf").toFile();
+        final XslFo xslFo = new XslFo(Collections.singletonList(foXslFile), docbookFile, false, outputFile);
+
+        // Act
+        xslFo.convertPdfWithXomAndSaxon(false);
+
+        // Assert
+        final List<ILoggingEvent> events = StaticAppender.getEvents();
+        assertThat(events, not(hasItem(hasProperty("message", containsString("Image not found")))));
+    }
+
+    @Test
+    void convertPdfWithXercesXinclude() throws Exception
+    {
+        // Arrange
+        final File outputFile = getTempDir("xerces").resolve(docbookFile.getName() + ".pdf").toFile();
+        final XslFo xslFo = new XslFo(Collections.singletonList(foXslFile), docbookFile, true, outputFile);
+
+        // Act
+        xslFo.convertPdfWithXercesInclude();
+
+        // Assert
+        final List<ILoggingEvent> events = StaticAppender.getEvents();
+        assertThat(events, not(hasItem(hasProperty("message", containsString("Image not found")))));
     }
 }
